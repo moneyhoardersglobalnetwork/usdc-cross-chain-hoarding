1cc725897d886f683a490371313cf72719bce049
moneyhoardermike <moneyhoardersglobal@gmail.com>
2024-06-02 21:49:10 -0400

    updated layout.tsx

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "USDC Cross-Chain Hoarding UI",
+  description: "A MHGN Hoarder Labs Product | Built with 🏗 Scaffold-ETH 2",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
